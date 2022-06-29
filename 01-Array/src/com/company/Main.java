c4a9a832368f37b16a4f6e390c74b82d217232ea
Shivam Sharma <meshivam81@gmail.com>
2022-06-29 06:54:13 +0530

    basic about arrays

--- 01-Array/src/com/company/Main.java
+++ 01-Array/src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// taking array as a user define
+        Scanner in = new Scanner(System.in);
+
+        int [] arr = new int[5];
+
+        for (int i = 0; i< arr.length; i++){
+
+           arr[i] = in.nextInt();
+        }
+        for (int i = 0; i< arr.length; i++){
+
+            System.out.println(arr[i]);
+        }
     }
 }
